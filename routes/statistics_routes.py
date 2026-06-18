5d16376d1db30c5da97558bec5993343983ad292
Polalise <97833838+Polalise@users.noreply.github.com>
2026-06-18 12:19:23 +0900

    Revert "Feature/statistics add to dev"

--- routes/statistics_routes.py
+++ routes/statistics_routes.py
@@ -1,20 +1,4 @@
-from flask import Blueprint, render_template
-
-from services.statistics_service import StatisticsService
+from flask import Blueprint
 
 
 statistics_bp = Blueprint("statistics", __name__, url_prefix="/statistics")
-
-db = None
-
-
-def init_db(database_instance):
-    global db
-    db = database_instance
-
-
-@statistics_bp.route("/", methods=["GET"])
-def statistics():
-    service = StatisticsService(db)
-    data = service.get_statistics()
-    return render_template("statistics.html", statistics=data)
