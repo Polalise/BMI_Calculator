5a3ac16dee6e456a5148acee4b4d9f027fb9e30a
Polalise <97833838+Polalise@users.noreply.github.com>
2026-06-18 12:47:04 +0900

    0618 유병현
    최종수정

--- tableSetting.sql
+++ tableSetting.sql
@@ -4,15 +4,21 @@ DEFAULT COLLATE utf8mb4_unicode_ci;
 
 USE test;
 
-CREATE TABLE IF NOT EXISTS member (
-    login_id VARCHAR(50) PRIMARY KEY,
-    password VARCHAR(50) NOT NULL,
-    name VARCHAR(50) NOT NULL
+CREATE TABLE IF NOT EXISTS members (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    activation_type INT DEFAULT 1
 );
+
+-- 2. BMI 데이터
+DROP TABLE IF EXISTS bmi_records;
 
 CREATE TABLE IF NOT EXISTS bmi_records (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    login_id VARCHAR(50),
+    member_id INT,
     weight FLOAT NOT NULL,
     height FLOAT NOT NULL,
     bmi FLOAT NOT NULL,
@@ -20,9 +26,10 @@ CREATE TABLE IF NOT EXISTS bmi_records (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+-- 3. 로그
 CREATE TABLE IF NOT EXISTS activity_logs (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    login_id VARCHAR(50),
+    member_id INT,
     action_type VARCHAR(20) NOT NULL,
     description VARCHAR(255),
     request_uri VARCHAR(255),
